8490e18a183739562888a1646bf5235cad56dbd2
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-02-25 16:11:01 +0100

    1er push db

--- app/page.js
+++ app/page.js
@@ -1,8 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 
 export default function Home() {
   return (
     
-    <div></div>
+    <div className="flex flex-col gap-4 ">
+      <p>installation next avec : <br />
+      npx create-next-app@latest</p>
+      <p>
+        Installation de l'orm prisma : Object Relation Mapping : <br />
+        npm i prisma <br />
+        npm i @prisma/client
+      </p>
+      <p>On initialise Prima : <br />
+      npx prisma init
+      </p>
+      <p>Dans supabase <br />
+      création DB <br />
+      project settings ➡️ database ➡️ Connection String <br />
+      on copie colle dans .env
+      
+      </p>
+      <p>Dans prisma/schema ➡️  on fait nos models de bases</p>
+      
+    </div>
   );
 }
